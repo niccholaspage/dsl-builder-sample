45908db27df9d98c16b3c848fd9b8aae7813a603
niccholaspage <niccholaspage@gmail.com>
2021-02-22 20:10:15 -0500

    Update sample to use new OPEN_COLLECTION_GENERIC modifier

--- src/main/java/com/example/effect/TargetedEffect.kt
+++ src/main/java/com/example/effect/TargetedEffect.kt
@@ -2,7 +2,8 @@ package com.example.effect
 
 import com.example.target.Target
 import com.example.target.group.TargetGroup
+import com.nicholasnassar.dslbuilder.annotation.BuilderModifier
 import com.nicholasnassar.dslbuilder.annotation.GenerateBuilder
 
-@GenerateBuilder
+@GenerateBuilder(BuilderModifier.OPEN_COLLECTION_GENERIC)
 open class TargetedEffect<T : Target>(private val targetGroups: List<TargetGroup<T>>)
