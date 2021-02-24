878211c833a4918a7951aeca711998a1179cd601
Nicholas Nassar <niccholaspage@gmail.com>
2021-02-24 14:49:58 -0500

    Fix sample issues

--- src/main/java/com/example/effect/LivingEntityTargetedEffect.kt
+++ src/main/java/com/example/effect/LivingEntityTargetedEffect.kt
@@ -1,8 +1,9 @@
 package com.example.effect
 
 import com.example.target.LivingEntityTarget
+import com.example.target.group.CasterTargetGroupSelection
 import com.nicholasnassar.dslbuilder.api.annotation.GenerateBuilder
 
 @GenerateBuilder
-class LivingEntityTargetedEffect : TargetedEffect<LivingEntityTarget>(emptyList()) {
+class LivingEntityTargetedEffect : TargetedEffect<LivingEntityTarget>(CasterTargetGroupSelection(), emptyList()) {
 }
--- src/main/java/com/example/effect/TargetedEffect.kt
+++ src/main/java/com/example/effect/TargetedEffect.kt
@@ -2,8 +2,9 @@ package com.example.effect
 
 import com.example.target.Target
 import com.example.target.group.TargetGroup
+import com.example.target.group.TargetGroupSelection
 import com.nicholasnassar.dslbuilder.api.annotation.BuilderModifier
 import com.nicholasnassar.dslbuilder.api.annotation.GenerateBuilder
 
 @GenerateBuilder(BuilderModifier.OPEN_COLLECTION_GENERIC)
-open class TargetedEffect<T : Target>(private val targetGroups: List<TargetGroup<T>>)
+open class TargetedEffect<T : Target>(private val selection: TargetGroupSelection<T>, private val targetGroups: List<TargetGroup<T>>)
