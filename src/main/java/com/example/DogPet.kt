9b576bfc06e79bd9274b02fa1e59314deae813ed
Nicholas Nassar <niccholaspage@gmail.com>
2021-02-24 13:07:29 -0500

    Rename Dog class for testing new smart naming of builders

--- src/main/java/com/example/DogPet.kt
+++ src/main/java/com/example/DogPet.kt
@@ -3,4 +3,4 @@ package com.example
 import com.nicholasnassar.dslbuilder.api.annotation.GenerateBuilder
 
 @GenerateBuilder
-class Dog(name: String, breed: String) : Pet(name, breed)
+class DogPet(name: String, breed: String) : Pet(name, breed)
