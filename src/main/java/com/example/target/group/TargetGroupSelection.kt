410a307a5743f4d6392005e930578bde0ff4f880
Nicholas Nassar <niccholaspage@gmail.com>
2021-02-25 01:59:37 -0500

    Fix generic type for TargetGroupSelection

--- src/main/java/com/example/target/group/TargetGroupSelection.kt
+++ src/main/java/com/example/target/group/TargetGroupSelection.kt
@@ -2,5 +2,4 @@ package com.example.target.group
 
 import com.example.target.Target
 
-interface TargetGroupSelection<in I: Target, T : Target> {
-}
+interface TargetGroupSelection<in I: Target, out T : Target>
