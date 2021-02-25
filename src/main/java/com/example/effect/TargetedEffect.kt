fe3a6b7f56aca7cdc3ff890ffaaa6be1457739ad
Nicholas Nassar <niccholaspage@gmail.com>
2021-02-25 08:33:57 -0500

    Remove @GenerateBuilder

--- src/main/java/com/example/effect/TargetedEffect.kt
+++ src/main/java/com/example/effect/TargetedEffect.kt
@@ -3,8 +3,7 @@ package com.example.effect
 import com.example.target.Target
 import com.example.target.group.TargetGroup
 import com.example.target.group.TargetGroupSelection
-import com.nicholasnassar.dslbuilder.api.annotation.BuilderModifier
 import com.nicholasnassar.dslbuilder.api.annotation.GenerateBuilder
 
-@GenerateBuilder(BuilderModifier.OPEN_COLLECTION_GENERIC)
+@GenerateBuilder
 open class TargetedEffect<in I: Target, in T : Target>(private val selection: TargetGroupSelection<I, T>, private val targetGroups: List<TargetGroup<I, T>>)
