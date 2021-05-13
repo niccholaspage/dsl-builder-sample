3de8e8c0552b11c6c7caf16ff255bbf6b1cc89e2
Nicholas Nassar <niccholaspage@gmail.com>
2021-05-13 13:21:42 -0400

    Update Kotlin to 1.5.0 and KSP to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.google.devtools.ksp") version "1.4.30-1.0.0-alpha02"
-    kotlin("jvm") version "1.4.30"
+    id("com.google.devtools.ksp") version "1.5.0-1.0.0-alpha10"
+    kotlin("jvm") version "1.5.0"
     idea
 }
 
