9d58a6da338db5bbec4222793d0c1d7e435902df
Nicholas Nassar <niccholaspage@gmail.com>
2023-01-06 16:23:06 -0500

    Replace "with" with using

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,8 @@ rootProject.name = "dsl-builder-sample"
 
 includeBuild("../dsl-builder") {
     dependencySubstitution {
-        substitute(module("com.nicholasnassar.dslbuilder:dsl-builder-api:0.0.1-SNAPSHOT")).with(project(":api"))
-        substitute(module("com.nicholasnassar.dslbuilder:dsl-builder-ksp:0.0.1-SNAPSHOT")).with(project(":processor"))
+        substitute(module("com.nicholasnassar.dslbuilder:dsl-builder-api:0.0.1-SNAPSHOT")).using(project(":api"))
+        substitute(module("com.nicholasnassar.dslbuilder:dsl-builder-ksp:0.0.1-SNAPSHOT")).using(project(":processor"))
     }
 }
 
